4bd9b2756332c9dcadf74a966a2ee1b02f93a2ad
JuanDa237 <juandavid200301@hotmail.com>
2022-10-19 22:37:49 -0500

    Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import { NavigationBar } from './components/navigationBar';
-
 import { Landing } from './pages/Landing';
 import { VehiclesList } from './pages/VehiclesList';
 import { VehicleDetail } from './components/VehicleDetail';
+import { Login } from './pages/Login';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <NavigationBar></NavigationBar>
         <Routes>
           <Route path="/" element={<Landing />}></Route>
           <Route path="/vehiculos" element={<VehiclesList />}></Route>
           <Route path="/vehiculo/:id" element={<VehicleDetail />}></Route>
+          <Route path="/login" element={<Login />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
--- src/pages/VehiclesList.tsx
+++ src/pages/VehiclesList.tsx
@@ -1,21 +1,25 @@
+import { NavigationBar } from '../components/navigationBar';
 import { VehicleCard } from '../components/VehicleCard';
 import { vehicles } from '../data/vehicle';
 
 export function VehiclesList() {
   return (
-    <div className="container-xl mt-5">
-      <div className="row">
-        <div className="h2">Reserva un vehiculo</div>
+    <>
+      <NavigationBar></NavigationBar>
+      <div className="container-xl mt-5">
+        <div className="row">
+          <div className="h2">Reserva un vehiculo</div>
+        </div>
+        <div className="row">
+          {vehicles.map((vehicle) => {
+            return (
+              <div className="col-4 mb-4">
+                <VehicleCard vehicle={vehicle}></VehicleCard>
+              </div>
+            );
+          })}
+        </div>
       </div>
-      <div className="row">
-        {vehicles.map((vehicle) => {
-          return (
-            <div className="col-4 mb-4">
-              <VehicleCard vehicle={vehicle}></VehicleCard>
-            </div>
-          );
-        })}
-      </div>
-    </div>
+    </>
   );
 }
