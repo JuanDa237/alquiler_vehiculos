f35dd8bd510191274bdb5d177afce77e3816ca34
JuanDa237 <juandavid200301@hotmail.com>
2022-12-02 15:56:55 -0500

    cambiar reserva

--- src/components/VehicleDetail.tsx
+++ src/components/VehicleDetail.tsx
@@ -1,28 +1,30 @@
+import React, { useState, useEffect } from 'react';
+
 import { useParams } from 'react-router-dom';
+
 import StepLabel from '@mui/material/StepLabel';
 import Step from '@mui/material/Step';
 import Stepper from '@mui/material/Stepper';
-import Typography from '@mui/material/Typography';
-import { useState } from 'react';
 import { Box } from '@mui/material';
 import Button from '@mui/material/Button';
-import React from 'react';
+
+import { IVehicle } from '../data/vehicle';
+import { VehicleApi } from '../api/vehicle.api';
 
 export function VehicleDetail() {
   const params = useParams();
-  const vehicle = {
-    id: 0,
-    brand: 'Nissan',
-    range: 'Media',
-    passengers: 4,
-    gearbox: 'automatico',
-    description:
-      'El revolucionario motor Skyactiv-G de 1,5L  de inyección directa, cuenta con innovadores desarrollos en los pistones e inyectores, reducción en la fricción y en el peso con una extraordinaria relación de compresión de 12 a 1. Entrega 108,6 caballos de potencia a 6.000 r.p.m. y un torque máximo de 14,38 Kg-m a 4.000 r.p.m. Transmisiones mecánica SKYACTIV MT y automática SKYACTIV-DRIVE, ambas de 6 velocidades que ofrecen cambios suaves y ágiles para un excelente desempeño y una dinámica de conducción superior.',
-    image:
-      'https://wieck-nissanao-production.s3.amazonaws.com/photos/62db2972c249b5cef536f7869ae44684eeb3b779/preview-928x522.jpg',
+  const [vehicle, setVehicle] = useState<IVehicle | undefined>();
+
+  useEffect(() => {
+    getVehicle(Number(params.id));
+  }, [params]);
+
+  const getVehicle = async (id: number) => {
+    const res = await VehicleApi.getVehicle(id);
+    setVehicle(res);
   };
 
-  const steps = ['Información Reserva', 'Información Personal', 'Finalización'];
+  const steps = ['Información Reserva', 'Información Personal', 'Confirmar'];
   const [activeStep, setActiveStep] = useState(0);
 
   const handleNext = () => {
@@ -33,8 +35,8 @@ export function VehicleDetail() {
     setActiveStep((prevActiveStep) => prevActiveStep - 1);
   };
 
-  return (
-    <div>
+  const CarCard = () => {
+    return (
       <div className="container-xl mt-5">
         <div className="row">
           <div className="col-12">
@@ -83,181 +85,214 @@ export function VehicleDetail() {
           </div>
         </div>
       </div>
-      <Box sx={{ width: '100%' }}>
-        <Stepper activeStep={activeStep}>
+    );
+  };
+
+  // Forms
+
+  const EscojerReserva = () => {
+    return (
+      <>
+        <div className="row">
+          <div className="col-12">
+            <h5>Escoje tu tipo de reserva</h5>
+          </div>
+          <div className="col-6">
+            <p className="fw-bold">Fecha de entrega: 20/02/2022</p>
+          </div>
+          <div className="col-6">
+            <p className="fw-bold">Fecha de devolucion: 29/02/2022</p>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-12">
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="radio"
+                name="flexRadioDefault"
+                id="flexRadioDefault1"
+              />
+              <label className="form-check-label" htmlFor="flexRadioDefault1">
+                <div className="row">
+                  <div className="col-2">Por Dias</div>
+                  <div className="col-10">
+                    <div className="">
+                      <label className="form-label">Kilometraje maximo</label>
+                      <input className="form-control" type="text" />
+                    </div>
+                  </div>
+                </div>
+              </label>
+            </div>
+          </div>
+          <div className="col-12">
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="radio"
+                name="flexRadioDefault"
+                id="flexRadioDefault2"
+                checked
+              />
+              <label className="form-check-label" htmlFor="flexRadioDefault2">
+                Kilometraje ilimitado
+              </label>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  };
+
+  const InformacionPersonal = () => {
+    return (
+      <form className="row row-cols-2">
+        <div className="col mb-2">
+          <label className="form-label">Tipo de documento</label>
+          <select className="form-select" aria-label="Default select example">
+            <option selected>C.C</option>
+            <option selected>Pasaporte</option>
+          </select>
+        </div>
+        <div className="col mb-2">
+          <label className="form-label">Número de documento</label>
+          <input className="form-control" type="text" />
+        </div>
+        <div className="col mb-2">
+          <label className="form-label">Tipo de documento</label>
+          <input className="form-control" type="text" />
+        </div>
+        <div className="col mb-2">
+          <label className="form-label">Empresa</label>
+          <input className="form-control" type="text" />
+        </div>
+        <div className="col mb-2">
+          <label className="form-label">Nombre Completo</label>
+          <input className="form-control" type="text" />
+        </div>
+        <div className="col mb-2">
+          <label className="form-label">Fecha de nacimiento</label>
+          <input className="form-control" type="text" />
+        </div>
+        <div className="col mb-2">
+          <label className="form-label">Genero</label>
+          <input className="form-control" type="text" />
+        </div>
+        <div className="col mb-2">
+          <label className="form-label">Telefono 1</label>
+          <input className="form-control" type="text" />
+        </div>
+        <div className="col mb-2">
+          <label className="form-label">Telefono 2</label>
+          <input className="form-control" type="text" />
+        </div>
+        <div className="col mb-2">
+          <label className="form-label">Correo electronico</label>
+          <input className="form-control" type="text" />
+        </div>
+        <div className="col mb-2">
+          <label className="form-label">Nivel escolaridad</label>
+          <input className="form-control" type="text" />
+        </div>
+        <div className="col mb-2">
+          <label className="form-label">Dirección</label>
+          <input className="form-control" type="text" />
+        </div>
+        <div className="col mb-2">
+          <label className="form-label">Dirección</label>
+          <input className="form-control" type="text" />
+        </div>
+        <div className="col mb-2">
+          <label className="form-label">Código postal</label>
+          <input className="form-control" type="text" />
+        </div>
+        <div className="col mb-2">
+          <label className="form-label">
+            Fecha de vencimiento de la licencia
+          </label>
+          <input className="form-control" type="text" />
+        </div>
+        <div className="col mb-2">
+          <label className="form-label">Número de licencia de conducción</label>
+          <input className="form-control" type="text" />
+        </div>
+      </form>
+    );
+  };
+
+  const Fin = () => {
+    return (
+      <>
+        <table>
+          <tr>
+            <td>
+              <b>Lugar de retiro</b>
+            </td>
+            <td>
+              <b>Lugar de devolución</b>
+            </td>
+          </tr>
+          <tr>
+            <td>20 de febrero de 2022</td>
+            <td>29 de febrero de 2022</td>
+          </tr>
+          <tr>
+            <td>Sede medellín - Calle 108 #65 e 42</td>
+            <td>Sede medellín - Calle 108 #65 e 42</td>
+          </tr>
+        </table>
+        <b>Datos personales</b>
+        <br />
+        Nombre: Pepito Perez Soza Dcoumento de identidad:10006279344 Licencia de
+        conducción: 111234543322
+        <br />
+        Correo Electronico: Pepito@correo.com Teléfono: 52342435
+        <br />
+        <b>Reserva: Kilometraje Ilimitado</b>
+        <br />
+        Valor día: 200.000$ Días: 9 Iva: 19% Descuento: 0
+      </>
+    );
+  };
+
+  return (
+    <>
+      <CarCard></CarCard>
+
+      <div className="container-fluid">
+        <Stepper className="my-4" activeStep={activeStep}>
           {steps.map((label, index) => {
-            const stepProps: { completed?: boolean } = {};
-            const labelProps: {
-              optional?: React.ReactNode;
-            } = {};
             return (
-              <Step key={label} {...stepProps}>
-                <StepLabel {...labelProps}>{label}</StepLabel>
+              <Step key={label}>
+                <StepLabel>{label}</StepLabel>
               </Step>
             );
           })}
         </Stepper>
-        {activeStep === steps.length ? (
-          <React.Fragment>
-            <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-              <Box sx={{ flex: '1 1 auto' }} />
-            </Box>
-          </React.Fragment>
+        {activeStep === 0 ? (
+          <EscojerReserva />
+        ) : activeStep === 1 ? (
+          <InformacionPersonal />
         ) : (
-          <React.Fragment>
-            <Typography sx={{ mt: 2, mb: 1 }}>Step {activeStep + 1}</Typography>
-            {activeStep === 0 ? (
-              <form>
-                <label>Escoge tu tipo de reserva</label>
-                <Box
-                  sx={{
-                    display: 'flex',
-                    flexDirection: 'row',
-                    justifyContent: 'space-around',
-                  }}
-                >
-                  <input type="date" name="" id="" />
-                  <input type="date" name="" id="" />
-                </Box>
-                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                  <span>
-                    <input type="radio" name="" id="" />
-                    <label>Por días</label> <label>Kilometraje máximo</label>
-                    <input type="Number" />
-                  </span>
-                  <span>
-                    <input type="radio" name="" id="" />
-                    <label>Kilometraje ilimitado</label>
-                  </span>
-                </Box>
-              </form>
-            ) : activeStep === 1 ? (
-              <form>
-                <label>Información Personal</label>
-                <Box
-                  sx={{
-                    display: 'grid',
-                    gridTemplateColumns: '50% 50%',
-                    padding: '20px',
-                  }}
-                >
-                  <span>
-                    <label>Tipo de documento</label>
-                    <select style={{ width: '100%' }} name="" id="">
-                      <option value="">C.C</option>
-                    </select>
-                  </span>
-                  <span>
-                    <label>Número de documento</label>
-                    <input style={{ width: '100%' }} type="text" />
-                  </span>
-                  <span>
-                    <label>Tipo de documento</label>
-                    <input style={{ width: '100%' }} type="text" />
-                  </span>
-                  <span>
-                    <label>Empresa</label>
-                    <input style={{ width: '100%' }} type="text" />
-                  </span>
-                  <span>
-                    <label>Nombre Completo</label>
-                    <input style={{ width: '100%' }} type="text" />
-                  </span>
-                  <span>
-                    <label>Fecha de nacimiento</label>
-                    <input style={{ width: '100%' }} type="text" />
-                  </span>
-                  <span>
-                    <label>Genero</label>
-                    <input style={{ width: '100%' }} type="text" />
-                  </span>
-                  <span>
-                    <label>Telefono 1</label>
-                    <input style={{ width: '100%' }} type="text" />
-                  </span>
-                  <span>
-                    <label>Telefono 2</label>
-                    <input style={{ width: '100%' }} type="text" />
-                  </span>
-                  <span>
-                    <label>Correo electronico</label>
-                    <input style={{ width: '100%' }} type="text" />
-                  </span>
-                  <span>
-                    <label>Nivel escolaridad</label>
-                    <input style={{ width: '100%' }} type="text" />
-                  </span>
-                  <span>
-                    <label>Dirección</label>
-                    <input style={{ width: '100%' }} type="text" />
-                  </span>
-                  <span>
-                    <label>Dirección</label>
-                    <input style={{ width: '100%' }} type="text" />
-                  </span>
-                  <span>
-                    <label>Código postal</label>
-                    <input style={{ width: '100%' }} type="text" />
-                  </span>
-                  <span>
-                    <label>Fecha de vencimiento de la licencia</label>
-                    <input style={{ width: '100%' }} type="text" />
-                  </span>
-                  <span>
-                    <label>Número de licencia de conducción</label>
-                    <input style={{ width: '100%' }} type="text" />
-                  </span>
-                </Box>
-              </form>
-            ) : (
-              <React.Fragment>
-                <table>
-                  <tr>
-                    <td>
-                      <b>Lugar de retiro</b>
-                    </td>
-                    <td>
-                      <b>Lugar de devolución</b>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>20 de febrero de 2022</td>
-                    <td>29 de febrero de 2022</td>
-                  </tr>
-                  <tr>
-                    <td>Sede medellín - Calle 108 #65 e 42</td>
-                    <td>Sede medellín - Calle 108 #65 e 42</td>
-                  </tr>
-                </table>
-                <b>Datos personales</b>
-                <br />
-                Nombre: Pepito Perez Soza Dcoumento de identidad:10006279344
-                Licencia de conducción: 111234543322
-                <br />
-                Correo Electronico: Pepito@correo.com Teléfono: 52342435
-                <br />
-                <b>Reserva: Kilometraje Ilimitado</b>
-                <br />
-                Valor día: 200.000$ Días: 9 Iva: 19% Descuento: 0
-              </React.Fragment>
-            )}
-            <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-              <Button
-                color="inherit"
-                disabled={activeStep === 0}
-                onClick={handleBack}
-                sx={{ mr: 1 }}
-              >
-                Back
-              </Button>
-              <Box sx={{ flex: '1 1 auto' }} />
-              <Button onClick={handleNext}>
-                {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-              </Button>
-            </Box>
-          </React.Fragment>
+          <Fin />
         )}
-      </Box>
-    </div>
+        <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+          <Button
+            color="inherit"
+            disabled={activeStep === 0}
+            onClick={handleBack}
+            sx={{ mr: 1 }}
+          >
+            Atras
+          </Button>
+          <Box sx={{ flex: '1 1 auto' }} />
+          <Button onClick={handleNext}>
+            {activeStep === steps.length - 1 ? 'Finalizar' : 'Siguiente'}
+          </Button>
+        </Box>
+      </div>
+    </>
   );
 }
